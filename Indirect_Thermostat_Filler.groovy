cd9d423eb971da8eace798858f5365f07ebb1f1a
rbaldwi3 <63027127+rbaldwi3@users.noreply.github.com>
2020-09-15 10:43:25 -0400

    Update Indirect_Thermostat_Filler.groovy
    Added support for switches as well as contact sensors and for either to be opposite sense.

--- Indirect_Thermostat_Filler.groovy
+++ Indirect_Thermostat_Filler.groovy
@@ -12,6 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ * This app can be used to fill a HVAC Zoning Status device to be used as a Thermostat input to an HVAC Zone when ThermostatOperatingState is being physically sensed.
  * version 0.2 - Initial Release
  */
 
@@ -27,12 +28,39 @@ definition(
 )
 
 preferences {
-    section ("Devices") {
-        input "stat", "device.HVACZoningStatus", required: true, title: "Indirect Thermostat"
-        input "heat", "capability.contactSensor", required: false, title: "Heating Call"
-        input "cool", "capability.contactSensor", required: false, title: "Cooling Call"
-        input "fan", "capability.contactSensor", required: false, title: "Fan Call"
-        input "temperature", "capability.temperatureMeasurement", required: false, title: "Temperature Source"
+    page(name: "pageOne", nextPage: "pageTwo", uninstall: true) {
+        section ("Label") {
+            label required: true, multiple: false
+        }
+        section ("Output") {
+            input "stat", "device.HVACZoningStatus", required: true, title: "Indirect Thermostat"
+        }
+        section ("Input Types") {
+            input(name: "input_type", type: "enum", required: true, title: "Input Type", options: ["Contact Sensor","Switch"])
+        }
+    }
+    page(name: "pageTwo", title: "Inputs", install: true, uninstall: true)
+}
+
+def pageTwo() {
+    dynamicPage(name: "pageTwo") {
+        section ("Inputs") {
+            switch ("$input_type") {
+                case "Contact Sensor":
+                    input "heat_contact", "capability.contactSensor", required: false, title: "Heating Call"
+                    input "cool_contact", "capability.contactSensor", required: false, title: "Cooling Call"
+                    input "fan_contact", "capability.contactSensor", required: false, title: "Fan Call"
+                    input "normally_closed", "bool", required: true, title: "True -> Open when call in progress", default: true
+                    break
+                case "Switch":
+                    input "heat_switch", "capability.switch", required: false, title: "Heating Call"
+                    input "cool_switch", "capability.switch", required: false, title: "Cooling Call"
+                    input "fan_switch", "capability.switch", required: false, title: "Fan Call"
+                    input "normally_off", "bool", required: true, title: "True -> On when call in progress", default: true
+                    break
+            }
+            input "temperature", "capability.temperatureMeasurement", required: false, title: "Temperature Source"
+        }
     }
 }
 
@@ -47,17 +75,35 @@ def updated() {
 
 def initialize() {
     log.debug("In Initialize()")
-    if (heat) {
-        subscribe(heat, "contact", callHandler)
+    if (heat_contact) {
+        subscribe(heat_contact, "contact", contact_callHandler)
     }
-    if (cool) {
-        subscribe(cool, "contact", callHandler)
+    if (cool_contact) {
+        subscribe(cool_contact, "contact", contact_callHandler)
     }
-    if (fan) {
-        subscribe(fan, "contact", callHandler)
+    if (fan_contact) {
+        subscribe(fan_contact, "contact", contact_callHandler)
+    }
+    if (heat_switch) {
+        subscribe(heat_switch, "switch", switch_callHandler)
+    }
+    if (cool_switch) {
+        subscribe(cool_switch, "switch", switch_callHandler)
+    }
+    if (fan_switch) {
+        subscribe(fan_switch, "switch", switch_callHandler)
     }
     if (temperature) {
+        tempHandler()
         subscribe(temperature, "temperature", tempHandler)
+        if (temperature.hasAttribute("heatingSetpoint")) {
+            heatsetHandler()
+            subscribe(temperature, " heatingSetpoint ", heatsetHandler)
+        }
+        if (temperature.hasAttribute("coolingSetpoint")) {
+            coolsetHandler()
+            subscribe(temperature, " coolingSetpoint ", coolsetHandler)
+        }
     }
 }
 
@@ -68,30 +114,109 @@ def tempHandler(evt=NULL) {
     stat.setTemperature(new_temp)
 }
 
-def callHandler(evt=NULL) {
-    log.debug("In callHandler()")
+def heatsetHandler(evt=NULL) {
+    log.debug("In heatsetHandler()")
+    def state = temperature.currentState("heatingSetpoint ")
+    new_temp = state.value as BigDecimal
+    stat. setHeatingSetpoint (new_temp)
+}
+
+def coolsetHandler(evt=NULL) {
+    log.debug("In coolsetHandler()")
+    def state = temperature.currentState("coolingSetpoint ")
+    new_temp = state.value as BigDecimal
+    stat. setCoolingSetpoint (new_temp)
+}
+
+def contact_callHandler(evt=NULL) {
+    log.debug("In contact_callHandler()")
     Boolean heat_call = false
-    if (heat) {
-        def state = heat.currentValue("contact")
+    if (heat_contact) {
+        def state = heat_contact.currentValue("contact")
         log.debug("heat = $state.value")
-        if ("$state.value" == "open") {
-           heat_call = true
+        if (normally_closed) {
+            if ("$state.value" == "open") {
+               heat_call = true
+            }
+        } else {
+            if ("$state.value" == "closed") {
+               heat_call = true
+            }
         }
     }
     Boolean cool_call = false
-    if (cool) {
-        def state = cool.currentValue("contact")
+    if (cool_contact) {
+        def state = cool_contact.currentValue("contact")
         log.debug("cool = $state.value")
-        if ("$state.value" == "open") {
-           cool_call = true
+        if (normally_closed) {
+            if ("$state.value" == "open") {
+               cool_call = true
+            }
+        } else {
+            if ("$state.value" == "closed") {
+               cool_call = true
+            }
         }
     }
     Boolean fan_call = false
-    if (fan) {
-        def state = fan.currentValue("contact")
+    if (fan_contact) {
+        def state = fan_contact.currentValue("contact")
         log.debug("fan = $state.value")
-        if ("$state.value" == "open") {
-           fan_call = true
+        if (normally_closed) {
+            if ("$state.value" == "open") {
+               fan_call = true
+            }
+        } else {
+            if ("$state.value" == "closed") {
+               fan_call = true
+            }
+        }
+    }
+    stat.update(heat_call, cool_call, fan_call)
+}
+
+def switch_callHandler(evt=NULL) {
+    log.debug("In switch_callHandler()")
+    Boolean heat_call = false
+    if (heat_switch) {
+        def state = heat_switch.currentValue("switch")
+        log.debug("heat = $state.value")
+        if (normally_off) {
+            if ("$state.value" == "on") {
+               heat_call = true
+            }
+        } else {
+            if ("$state.value" == "off") {
+               heat_call = true
+            }
+        }
+    }
+    Boolean cool_call = false
+    if (cool_switch) {
+        def state = cool_switch.currentValue("switch")
+        log.debug("cool = $state.value")
+        if (normally_off) {
+            if ("$state.value" == "on") {
+               cool_call = true
+            }
+        } else {
+            if ("$state.value" == "off") {
+               cool_call = true
+            }
+        }
+    }
+    Boolean fan_call = false
+    if (fan_switch) {
+        def state = fan_switch.currentValue("switch")
+        log.debug("fan = $state.value")
+        if (normally_off) {
+            if ("$state.value" == "on") {
+               fan_call = true
+            }
+        } else {
+            if ("$state.value" == "off") {
+               fan_call = true
+            }
         }
     }
     stat.update(heat_call, cool_call, fan_call)
